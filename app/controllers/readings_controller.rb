56918623f2338ae59ccc540c0b46f123ffade72f
Radek <radek.stasiak@gmail.com>
2019-07-23 22:41:03 +0100

    List readings

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -3,7 +3,22 @@ class ReadingsController < ApplicationController
 	def new
 
 	end
+
+	def index
+		@readings = Reading.all
+	end
 	def create
-  		render plain: params[:reading].inspect
+  		 @reading = Reading.new(reading_params)
+  		 @reading.save
+  		 redirect_to @reading
+  	end
+
+  	def show
+		@reading = Reading.find(params[:id])
+  	end
+
+  private
+  	def reading_params
+    	params.require(:reading).permit(:name, :moisture_level_status,:soil_moisture_status,:water_pump_status)
   	end
 end
---
+++ db/schema.rb
@@ -0,0 +1,24 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_07_23_212623) do
+
+  create_table "readings", force: :cascade do |t|
+    t.text "name"
+    t.integer "moisture_level_status"
+    t.text "soil_moisture_status"
+    t.integer "water_pump_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
